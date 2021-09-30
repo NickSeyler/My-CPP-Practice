285c90a00c4f43430bc473be1537d5b1c064a6b7
Nick Seyler <49881154+NickSeyler@users.noreply.github.com>
2021-09-30 12:25:11 -0400

    Add files via upload

--- CPP-Set-2/set/set.cpp
+++ CPP-Set-2/set/set.cpp
@@ -8,114 +8,114 @@
 
 set::set()
 {
-   used = 0;
+    used = 0;
 }
 
 void set::insert(const value_type& entry)
 {
-   if (find(entry) == used)
-   {
-      assert (used < CAPACITY);
-      data[used] = entry;
-      used++;
-   }
-   else
-      return;
+    if (find(entry) == used)
+    {
+        assert(used < CAPACITY);
+        data[used] = entry;
+        used++;
+    }
+    else
+        return;
 }
 
 void set::remove(const value_type& entry)
 {
-   if (find(entry) != used)
-   {
-      data[find(entry)] = 0;
-      used --;
-   }
+    if (find(entry) != used)
+    {
+        data[find(entry)] = 0;
+        used--;
+    }
 }
 
 set::size_type set::size() const
 {
-   return used;
+    return used;
 }
 
 bool set::contains(const value_type& entry) const
 {
-   if (find(entry) != used)
-      return true;
-   else
-      return false;
+    if (find(entry) != used)
+        return true;
+    else
+        return false;
 }
 
-set set_union (const set& s1, const set& s2)
+set set_union(const set& s1, const set& s2)
 {
-   set s3;
-   
-   for (int i = 0; i < s1.used; i++)
-      s3.insert(s1.data[i]);
-   
-   for (int i = 0; i < s2.used; i++)
-      s3.insert(s2.data[i]);
-   
-   return s3;
+    set s3;
+
+    for (int i = 0; i < s1.used; i++)
+        s3.insert(s1.data[i]);
+
+    for (int i = 0; i < s2.used; i++)
+        s3.insert(s2.data[i]);
+
+    return s3;
 }
 
 set set_intersection(const set& s1, const set& s2)
 {
-   set s3;
-   
-   for (int i = 0; i < s1.used; i++)
-      if (s2.find(s1.data[i]) != s2.used)
-         s3.insert(s1.data[i]);
-   
-   return s3;
+    set s3;
+
+    for (int i = 0; i < s1.used; i++)
+        if (s2.find(s1.data[i]) != s2.used)
+            s3.insert(s1.data[i]);
+
+    return s3;
 }
 
 set set_difference(const set& s1, const set& s2)
 {
-   set s3;
-   
-   for (int i = 0; i < s1.used; i++)
-      if (s2.find(s1.data[i]) == s2.used)
-         s3.insert(s1.data[i]);
-   
-   return s3;
+    set s3;
+
+    for (int i = 0; i < s1.used; i++)
+        if (s2.find(s1.data[i]) == s2.used)
+            s3.insert(s1.data[i]);
+
+    return s3;
 }
 
-bool is_subset (const set& s1, const set& s2)
+bool is_subset(const set& s1, const set& s2)
 {
-   for (int i = 0; i < s1.used; i++)
-      if (s2.find(s1.data[i]) == s2.used)
-         return false;
-         
-   return true;
+    for (int i = 0; i < s1.used; i++)
+        if (s2.find(s1.data[i]) == s2.used)
+            return false;
+
+    return true;
 }
 
 bool operator == (const set& s1, const set& s2)
 {
-   if (s1.size() != s2.size())
-      return false;
-   else
-   {  
-      for (int i = 0; i < s1.used; i++)
-         if (s2.find(s1.data[i]) == s2.used)
-            return false;
-       
-      return true;
-   }
+    if (s1.size() != s2.size())
+        return false;
+    else
+    {
+        for (int i = 0; i < s1.used; i++)
+            if (s2.find(s1.data[i]) == s2.used)
+                return false;
+
+        return true;
+    }
 }
 
 std::ostream& operator << (std::ostream& output, const set& s)
 {
-   for (int i = 0; i < s.used; i++)
-      output << s.data[i] << " ";
-   
-   return output;
+    for (int i = 0; i < s.used; i++)
+        output << s.data[i] << " ";
+
+    return output;
 }
 
 set::size_type set::find(const value_type& entry) const
 {
-   for (int i = 0; i < used; i++)
-      if (data[i] == entry)
-         return i;
-         
-   return used;
+    for (int i = 0; i < used; i++)
+        if (data[i] == entry)
+            return i;
+
+    return used;
 }
--- CPP-Set-2/set/set.h
+++ CPP-Set-2/set/set.h
@@ -1,3 +1,7 @@
+//Author:   Nick Seyler
+//Date:     2/7/17
+//File:     set_test.h
+
 #ifndef _SET_H
 #define _SET_H
 
@@ -7,61 +11,49 @@
 class set
 {
 public:
-  typedef int value_type;
-  typedef std::size_t size_type;
-  static const size_type CAPACITY = 30;
+	typedef int value_type;
+	typedef std::size_t size_type;
+	static const size_type CAPACITY = 30;
 
-  set();
-  // postcondition: empty set has been created
-  // efficiency: N
-  
-  void insert (const value_type& entry);
-  // precondition: if entry is not in the set, set is not full
-  // postcondition: entry is in the set
-  // efficiency: O(n)
+	set();
+	// postcondition: empty set has been created
 
-  void remove (const value_type& entry);
-  // postcondition: entry is not in the set
-  // efficiency: O(n)
+	void insert(const value_type& entry);
+	// precondition: if entry is not in the set, set is not full
+	// postcondition: entry is in the set
 
-  size_type size() const;
-  // postcondition: number of elements in the set has been returned
-  // efficiency: N 
+	void remove(const value_type& entry);
+	// postcondition: entry is not in the set
 
-  bool contains (const value_type& entry) const;
-  // postcondition: whether entry is in the set has been returned
-  // efficiency: O(n)
+	size_type size() const;
+	// postcondition: number of elements in the set has been returned
 
-  friend set set_union (const set& s1, const set& s2);
-  // postcondition: union of s1 & s2 has been returned
-  // efficiency: O(n)
+	bool contains(const value_type& entry) const;
+	// postcondition: whether entry is in the set has been returned
 
-  friend set set_intersection (const set& s1, const set& s2);
-  // postcondition: intersection of s1 & s2 has been returned
-  // efficiency: O(n^2)
+	friend set set_union(const set& s1, const set& s2);
+	// postcondition: union of s1 & s2 has been returned
 
-  friend set set_difference (const set& s1, const set& s2);
-  // postcondition: difference of s1 - s2 has been returned
-  // efficiency: O(n^2)
+	friend set set_intersection(const set& s1, const set& s2);
+	// postcondition: intersection of s1 & s2 has been returned
 
-  friend bool is_subset (const set& s1, const set& s2);
-  // postcondition: returned whether s1 is a subset of s2
-  // efficiency: O(n^2)
+	friend set set_difference(const set& s1, const set& s2);
+	// postcondition: difference of s1 - s2 has been returned
 
-  friend bool operator == (const set& s1, const set& s2);
-  // postcondition: returned whether s1 & s2 are equal
-  // efficiency: O(n^2)
+	friend bool is_subset(const set& s1, const set& s2);
+	// postcondition: returned whether s1 is a subset of s2
 
-  friend std::ostream& operator << (std::ostream& output, const set& s);
-  // postcondition: s has been displayed on output
-  // efficiency: O(n)
+	friend bool operator == (const set& s1, const set& s2);
+	// postcondition: returned whether s1 & s2 are equal
+
+	friend std::ostream& operator << (std::ostream& output, const set& s);
+	// postcondition: s has been displayed on output
 
 private:
-  size_type find (const value_type& entry) const;
-  // returned location of entry in the set if entry is in the set - used otherwise
-  // efficiency: O(n)
-  value_type data[CAPACITY];
-  size_type used;
+	size_type find(const value_type& entry) const;
+	// returned location of entry in the set if entry is in the set - used otherwise
+	value_type data[CAPACITY];
+	size_type used;
 };
 
 
